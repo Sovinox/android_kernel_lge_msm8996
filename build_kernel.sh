e13810963db797e76b3e0d1a8c26d08160b32707
Paul Cho <sovinox9@gmail.com>
2018-03-01 20:40:08 -0500

    Allow in-tree compilation

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE=/home/sovinox/build/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
-make clean
-make mrproper
+#make clean
+#make mrproper
 make lineageos_h830_defconfig 
-make -s -j5
+make -j5
